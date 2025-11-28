4866a823893379e813e534353c5d06c12d6e6376
muffadalis <muffyj@gmail.com>
2025-11-29 12:54:37 +1300

    pool fences done

--- provista.ps1
+++ provista.ps1
@@ -1,9 +1,26 @@
+function Convert-PathFormat {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+    
+    # Remove "static\" from the beginning if it exists
+    $path = $path -replace '^static\\', ''
+    
+    # Replace all backslashes with forward slashes
+    $path = $path -replace '\\', '/'
+    
+    # Add leading forward slash if not present
+    if (-not $path.StartsWith('/')) {
+        $path = '/' + $path
+    }
+    
+    return $path
+}
+
 # List of all img URLs (paste yours here)
 $imgUrls = @(
-"https://provista.co.nz/wp-content/uploads/2022/09/provista-new-logo_2022-72dpi.png"
-"https://provista.co.nz/wp-content/uploads/2018/07/framelessglass-square.jpg"
-"https://provista.co.nz/wp-content/themes/dt-the7/images/the7-chevron-down.svg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa-1.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/pool3.jpg"
 "https://provista.co.nz/wp-content/uploads/2018/08/facebook-icon2.png"
 "https://provista.co.nz/wp-content/uploads/2018/08/instagram-icon2.png"
 "https://provista.co.nz/wp-content/uploads/2022/09/provista-new-logo_2022-72dpi.png"
@@ -11,86 +28,84 @@ $imgUrls = @(
 "https://provista.co.nz/wp-content/plugins/js_composer/assets/lib/vendor/dist/lightbox2/dist/images/next.png"
 "https://provista.co.nz/wp-content/plugins/js_composer/assets/lib/vendor/dist/lightbox2/dist/images/loading.gif"
 "https://provista.co.nz/wp-content/plugins/js_composer/assets/lib/vendor/dist/lightbox2/dist/images/close.png"
-"data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="
-"https://provista.co.nz/wp-content/uploads/2018/08/aaa2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaa1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaa2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaa3.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa3.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa4.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa6.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa7.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaa1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaa2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaa3.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa3.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa4.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa6.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaa7.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa3.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa3as-1-1024x682.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/aaaaa4.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/ALaker6.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/ddddd.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/DSCF0063-1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/fffff.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/fffff11.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/FlessChanDeck-.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Flesschannel-1024x686.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/FlessChanStairs-1024x768.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/FlessInt.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/FlessS-off-stairs-768x1024.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/FlessStand-off.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-glass-with-powder-coated-continuos-rail.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-glass-with-powder-coated-continuos-rail2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-side-fixed-20.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-side-fixed-21.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-side-fixed-stairs-aa.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-side-fixed-stairs-with-stainless-handrail.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-side-fixed-stairs.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-side-fixed-stairsa1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-side-fixed-stairsa12.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-with-Stainless-rail.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Frameless12mmSO1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/framelesspic1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/framelesspic3.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/framelesspic4.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Framless-side-fixed-channel1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/gggg12.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/IMG_4684-1024x661.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/NSGCFless34.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-frameless-channel-2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-frameless-channel-3.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Side-fixed-frameless-channel-stairs-1024x682.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-frameless-channel1a.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-frameless-channel1bb.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-frameless-standoffs.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-frameless-standoffs1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-frameless-standoffs1a.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-frameless-standoffs11.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Side-fixed-frameless-with-internal-shelf.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Side-fixed-frameless.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/side-fixed-framless-channel2-1024x781.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Side-fixed-framless-standoffs.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Side-fixed-framless.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Side-fixed-standf-offs.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/SpigGB1.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/sssss.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Stair-side-fixed-2.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/top-fixed-frameless-channel.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Top-fixed-framless-channel.jpg"
-"https://provista.co.nz/wp-content/uploads/2018/08/Vista-side-fixed-21.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/07/111b.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111a.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111b.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111c.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111c1.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111d.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111e.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111f.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/20170502_102541.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/20170502_102556.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/20171222_095733.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/20171222_095733a.jpg"
 "https://www.gstatic.com/recaptcha/api2/logo_48.png"
+"https://provista.co.nz/wp-content/uploads/2018/08/111.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111a.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111b.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111c.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111c1.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111d.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111e.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/111f.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/20170502_102541.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/20170502_102556.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/20171222_095733.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/20171222_095733a.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/ALaker4.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/ALaker5.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/aluminum-6mm-glass-1024x621.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Boord214-002.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Boord214-003.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Boord214-007.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Euro-Slat-and-vista-pool-fence.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/EuroPool2.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Euroslatvistapool.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/FF33-Okura-003.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/FF69new-018.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/FF69new-023.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/FlessGateVistaSPG2.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/FlessSpigPool.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Framed-123c-1024x605.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-glass-clamp.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Frameless-Ultimate-Spigot12.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Freshfields69-001.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/IMG_1444.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/IMG_2591-Copy.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/IMG_2593.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/IMG_2594.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/IMG_2595.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Jan27th-021.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/NewSpig1-1024x768.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Oakford9Reva.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/OldNth338Spigot-013.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/pool-spigots-framless-glass.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/PoolBroc.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Reva-spa-1024x768.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Reva12-1024x578.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Reva12cJPG-1024x606.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Reva121-1024x512.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Reva1112-1024x739.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/reva1203-1024x705.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Reva1211-1024x659.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Side-fixed-frameless-glass-pool-fence.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/SPG-Gate1.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Spig-Pool.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/SpigotPool2.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/SS-Spigot-Pool.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/swim-pool-fence.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Tudor-pool-1024x682.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Vetro-framed-gate-1024x682.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/vista-euroslat1.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Vista-pool-3-1024x682.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/Vista-side402-1024x921.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/VistaEuroSPG3.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/VistaPoolFramedGate.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/VistaPoolFramedhheee-1024x689.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/VistaPoolFramedhhee-1024x678.jpg"
+"https://provista.co.nz/wp-content/uploads/2018/08/VistaPoolFramedhhe-1024x682.jpg"
 )
 
 # Filter only REAL product images
@@ -108,7 +123,7 @@ $filtered = $imgUrls |
 Write-Host "Filtered image count:" $filtered.Count
 
 # Save folder
-$folder = "static\assets\img\balustrades\semi-frameless-glass-balustrade"
+$folder = "static\assets\img\balustrades\pool-fences"
 New-Item -ItemType Directory -Force -Path $folder | Out-Null
 
 # YAML OUTPUT HEADER
@@ -121,11 +136,12 @@ foreach ($url in $filtered) {
 
     # Download file
     # Write-Host "Downloading $fileName"
-    Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
+    # Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
 
     # YAML FORMATTED LINE
-    # Write-Host "  - "$dest""
+    Write-Host "  - "$(Convert-PathFormat -Path $dest)""
 }
 
 Write-Host ""
 Write-Host "Done. Saved to $folder"
+
